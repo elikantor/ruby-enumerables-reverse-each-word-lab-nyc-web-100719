d5bd4ca7543c97461d70541479af553ff00dd71b
Eli Kantor <eli.kantor@outlook.com>
2019-09-30 16:44:47 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,31 @@
+def reverse_each_word(string)
+  newarray = string.split(" ")
+  
+  newarray.each do |word|
+    print "#{word} ".reverse
+  end
+
+
+end
+
+reverse_each_word("Hello there, and how are you?")
+
+
+
+
+
+
+def reverse_each_word(string)
+  newarray = string.split(" ")
+  
+  newarray.collect do |word|
+    print "#{word} ".reverse
+  end
+
+
+end
+
+reverse_each_word("Hello there, and how are you?")
+
+
+  
